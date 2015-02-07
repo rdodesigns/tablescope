4452428353c2f232292072556615605b55a359dd
Ryan Orendorff <ryan@rdodesigns.com>
2015-02-06 21:29:36 -0800

    Add new project files, default from XCode.

---
+++ tablescope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tablescope
+//
+//  Created by Ryan Orendorff on 6/2/15.
+//  Copyright (c) 2015 cellscope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
