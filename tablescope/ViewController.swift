d0cccd20e307d7b36aae1773182e78c6611eab4a
Ryan Orendorff <ryan@rdodesigns.com>
2015-04-29 12:30:57 -0700

    Set the app to trigger based on hour.
    Also starts in the appropriate mode (either during the day or night).
    
    `beginSession` and `endSession` are replaced with `daySession` and
    `nightSession` respectively.

--- tablescope/ViewController.swift
+++ tablescope/ViewController.swift
@@ -53,36 +53,91 @@ class ViewController: UIViewController {
                         if err != nil {
                             println("error: \(err?.localizedDescription)")
                         }
-
-                        beginSession()
                     }
                 }
             }
         }
 
+
+        assert(sleepTimeStart.hour > sleepTimeStop.hour ||
+                  (sleepTimeStart.hour == sleepTimeStop.hour &&
+                   sleepTimeStart.minute > sleepTimeStop.minute),
+               "Sleep start and stop are in the wrong order!")
+
+        // When starting the app, we are going to figure out if we are
+        // in a night session time or a day session time.
+        let now = NSDate()
+        let calendar = NSCalendar(
+                        calendarIdentifier: NSCalendarIdentifierGregorian)
+
+        // The components and [start/stop]Today are specified for a start
+        // and stop time that are both in the current day. This situation
+        // only occurs if we are before the stop time on the current day.
+        // Otherwise we will need to either add a day to the start or the
+        // end dates in order to trigger them correctly.
+        let start_components = calendar!.components(.CalendarUnitYear |
+            .CalendarUnitMonth | .CalendarUnitDay, fromDate: now)
+        start_components.hour = sleepTimeStart.hour
+        start_components.minute = sleepTimeStart.minute
+
+        let startToday = calendar!.dateFromComponents(start_components)
+
+
+        let stop_components = calendar!.components(.CalendarUnitYear |
+            .CalendarUnitMonth | .CalendarUnitDay, fromDate: startToday!)
+        stop_components.hour = sleepTimeStop.hour
+        stop_components.minute = sleepTimeStop.minute
+
+        let stopToday = calendar!.dateFromComponents(stop_components)
+
+        // Ordering is a bit strange, but .OrderedAscending is when today
+        // is before the queried time
+        let order_start = calendar!.compareDate(now, toDate: startToday!,
+            toUnitGranularity: .CalendarUnitMinute)
+        let order_stop = calendar!.compareDate(now, toDate: stopToday!,
+            toUnitGranularity: .CalendarUnitMinute)
+
+        // Here we will start the manipulations of the [start/stop]Today
+        // dates to reflect the correct days to trigger on. This case 
+        // corresponds to being in the morning night session.
+        var startDate = startToday!
+        var stopDate = stopToday!
+
+        // Figure out if we are in a day or night session, and start the
+        // app in the correct configuration when first launched.
+        if now < startToday && now >= stopToday {
+            daySession()
+
+            // We are during the day so we need to move the stop date to 
+            // trigger tomorrow for the first time (today's time has passed)
+            stopDate = stopToday!.dateByAddingTimeInterval(year_in_sec)
+        } else {
+            nightSession()
+
+            // We are during the later night period so we need to move both
+            // the start and the stop stop date to trigger tomorrow for the
+            // first time (today's time has passed)
+            if now >= startToday {
+                startDate = startToday!.dateByAddingTimeInterval(year_in_sec)
+                stopDate = stopToday!.dateByAddingTimeInterval(year_in_sec)
+            }
+
+        }
+
+        // Finally set the timers.
         Timer.scheduledTimerWithTimeInterval(
-            NSDate().dateByAddingTimeInterval(5), interval: 1,
-            repeats: false, f:
-            {
-                self.endSession()
-                UIScreen.mainScreen().brightness = CGFloat(0)
-                println("Setting the display to lowest brightness, " +
-                        "disabling camera capture.")
-            })
+            startDate, interval: year_in_sec,
+            repeats: true, f: { self.nightSession() })
 
         Timer.scheduledTimerWithTimeInterval(
-            NSDate().dateByAddingTimeInterval(10), interval: 1,
-            repeats: false, f:
-            {
-                self.beginSession()
-                UIScreen.mainScreen().brightness = CGFloat(1)
-                println("Setting the display to full brightness, " +
-                        "enabling camera capture.")
-            })
+            stopDate, interval: year_in_sec,
+            repeats: true, f: { self.daySession() })
 
-    }
+    } // end viewDidLoad
 
-    func beginSession() {
+    func daySession() -> Void {
+        UIScreen.mainScreen().brightness = CGFloat(1)
+
         if !captureSession.running{
             previewLayer =
                 AVCaptureVideoPreviewLayer(session: captureSession)
@@ -93,7 +148,9 @@ class ViewController: UIViewController {
         }
     }
 
-    func endSession(){
+    func nightSession() -> Void {
+        UIScreen.mainScreen().brightness = CGFloat(0)
+
         if captureSession.running{
             self.previewLayer?.removeFromSuperlayer()
             captureSession.stopRunning()
